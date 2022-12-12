4b2712b43b97341cdbdbe48a97224e2816441b27
Artem Prudov <artyom1911@gmail.com>
2022-12-12 23:39:09 +0300

    task1 edit

--- Lesson 2/task 1.py
+++ Lesson 2/task 1.py
@@ -4,3 +4,20 @@
 
 - 6782 -> 23
 - 0.56 -> 11'''
+
+a = input('Введите число = ')
+c = 0
+if type(a) == int:
+    while a > 0:
+        b = a % 10
+        c = b + c
+        a = a // 10
+        
+
+if type(a) == float:
+    while a > 0:
+        b = a % 10
+        c = b + c
+        a = a // 10
+        
+print (c)
